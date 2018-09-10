264bd4b6268f531658cfd2420030ccacbf55497b
ztcao <ztcao@3ti.us>
2018-09-10 17:51:14 +0800

    上传身份证信息 UI修改

--- pages/ticketpayinfo/ticketpayinfo.js
+++ pages/ticketpayinfo/ticketpayinfo.js
@@ -11,10 +11,13 @@ Page({
       { value: '2', name: '外地身份证', checked: false},
     ],
     ticketInfo:{
+      idCardType:1 ,
       prince:100 ,
       idcard:""  ,
       tel:""     ,
-    }
+    },
+    url:'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg' ,
+    isHidden:true 
   },
 
   gopay:function(){
@@ -61,5 +64,48 @@ Page({
       'ticketInfo.tel': e.detail.value
     })
   },
+
+  upload:function(){
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        //上传图片
+        wx.uploadFile({
+          url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
+          filePath: tempFilePaths[0],
+          name: 'file',
+          formData: {
+            'user': 'test'
+          },
+          success: function (res) {
+            var data = res.data
+            //do something
+          }
+        })
+      }
+    })
+  },
+
+  radioChange: function (e) {
+    let value = e.detail.value 
+    if(value == 1){
+      this.setData({
+        isHidden: true  ,
+        'ticketInfo.idCardType': e.detail.value
+      })
+    }
+    else{
+      this.setData({
+        isHidden: false,
+        'ticketInfo.idCardType': e.detail.value
+      })
+    }
+  }
+
+  
 })
 
