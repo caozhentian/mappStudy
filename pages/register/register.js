83cf10ee46732adff9d19932609714429d55feaa
ztcao <ztcao@3ti.us>
2018-09-17 12:23:47 +0800

    注册页面 UI

---
+++ pages/register/register.js
@@ -0,0 +1,114 @@
+const paymentUrl = require('../../config').paymentUrl
+const identitycode = require('../../utils/identitycode.js')
+const phonevaliate = require('../../utils/phonevaliate.js')
+
+var app = getApp()
+
+Page({
+  data: {
+    idcard: "132330198109142478",
+    tel: "13186075334",
+    password:"1" ,
+    confirmPassword:"123" ,
+  },
+
+  register:function(){
+    let idcard = this.data.idcard
+    if (idcard.length == 0 || idcard == undefined){
+      wx.showToast({
+        title: '请输入身份证',
+        icon:'none',
+      })
+      return 
+    }
+    //校验身份证
+    if (!identitycode.identityCodeValid(idcard)){
+      wx.showToast({
+        title: '身份证输入有误',
+        icon: 'none',
+      })
+        return 
+    }
+    let tel = this.data.tel
+    if (tel.length == 0 || tel == undefined) {
+      wx.showToast({
+        title: '请输入手机号码',
+        icon: 'none',
+      })
+      return
+    }
+    if (!phonevaliate.isPhoneAvailable(tel)){
+      wx.showToast({
+        title: '手机号输入有误',
+        icon: 'none',
+      })
+      return
+    }
+    let password = this.data.password
+    if (password == '' || password == undefined) {
+      wx.showToast({
+        title: '请输入密码',
+        icon: 'none',
+      })
+      return
+    }
+    if (password.length < 6 ) {
+      wx.showToast({
+        title: '密码长度小于6',
+        icon: 'none',
+      })
+      return
+    }
+    let confirmPassword = this.data.confirmPassword
+    if (confirmPassword == '' || confirmPassword == undefined) {
+      wx.showToast({
+        title: '请输入确认密码',
+        icon: 'none',
+      })
+      return
+    }
+    if (confirmPassword.length < 6) {
+      wx.showToast({
+        title: '确认密码长度小于6',
+        icon: 'none',
+      })
+      return
+    }
+    if (password != confirmPassword){
+      wx.showToast({
+        title: '两次输入的密码不一致',
+        icon: 'none',
+      })
+      return
+    }
+    //关闭当前页面
+    wx.navigateBack({
+      
+    })
+  },
+
+  bindIdcardKeyInput: function (e) {
+    this.setData({
+      'idcard': e.detail.value
+    })
+  },
+
+  bindTelKeyInput: function (e) {
+    this.setData({
+      'tel': e.detail.value
+    })
+  },
+
+  bindPasswordKeyInput: function (e) {
+    this.setData({
+      'password': e.detail.value
+    })
+  },
+  bindConfirmPasswordKeyInput: function (e) {
+    this.setData({
+      'confirmPassword': e.detail.value
+    })
+  },
+
+})
+
