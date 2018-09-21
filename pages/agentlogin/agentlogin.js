ccd50cb8ae9adb734f697f6a19c54bb02ca5ae91
ztcao <ztcao@3ti.us>
2018-09-21 14:01:45 +0800

    普通用户 代理商 登录功能

---
+++ pages/agentlogin/agentlogin.js
@@ -0,0 +1,72 @@
+const paymentUrl = require('../../config').paymentUrl
+const identitycode = require('../../utils/identitycode.js')
+const phonevaliate = require('../../utils/phonevaliate.js')
+var network_util = require('../../utils/network_util.js');
+var app = getApp()
+
+Page({
+  data: {
+    idcard: "132330198109142478",
+    tel: "13186075334",
+    password:"123456" ,
+    confirmPassword:"123456" ,
+  },
+
+  register:function(){
+    let idcard = this.data.idcard
+    if (idcard.length == 0 || idcard == undefined){
+      wx.showToast({
+        title: '请输入账号',
+        icon:'none',
+      })
+      return 
+    }
+   
+    
+    let password = this.data.password
+    if (password == '' || password == undefined) {
+      wx.showToast({
+        title: '请输入密码',
+        icon: 'none',
+      })
+      return
+    }
+    
+    network_util._post1('/abdn', this.data,
+      function (res) {
+        //关闭当前页面
+        wx.navigateBack({
+
+        })
+      },
+      function (res) {
+        console.log(res);
+      })
+   
+  },
+
+  bindIdcardKeyInput: function (e) {
+    this.setData({
+      'idcard': e.detail.value
+    })
+  },
+
+  bindTelKeyInput: function (e) {
+    this.setData({
+      'tel': e.detail.value
+    })
+  },
+
+  bindPasswordKeyInput: function (e) {
+    this.setData({
+      'password': e.detail.value
+    })
+  },
+  bindConfirmPasswordKeyInput: function (e) {
+    this.setData({
+      'confirmPassword': e.detail.value
+    })
+  },
+
+})
+
